acd821219005105e3e9aafba9221c72bcb3e9bf4
HugoRaak <rathle.hugo@gmail.com>
2023-09-07 22:39:51 +0900

    feat: excerpt

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 /**
  * @mixin IdeHelperPost
@@ -24,5 +25,12 @@ class Post extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
+    }
+
+    public function excerpt(string $string, int $limit=100): ?string
+    {
+        $excerpt = Str::limit($string, $limit);
+        if($lastSpace = strrpos($excerpt, ' ')) return substr($excerpt, 0, $lastSpace) . '...';
+        return $excerpt;
     }
 }
--- resources/views/post/card.blade.php
+++ resources/views/post/card.blade.php
@@ -3,7 +3,7 @@
         <div class="card-title">
             <a href="{{route('post.show', ['slug' => $post->slug, 'post' => $post])}}"><h4>{{$post->title}}</h4></a>
         </div>
-        <p class="card-text">{{nl2br($post->content)}}</p> {{--TODO: exerpt--}}
+        <p class="card-text">{{nl2br($post->excerpt($post->content))}}</p>
         <div class="card-footer">
             <p class="text-muted text-center">posté {{Carbon\Carbon::parse($post->created_at)->ago()}}</p>
         </div>
